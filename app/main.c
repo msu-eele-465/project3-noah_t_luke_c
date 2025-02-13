ee18f95b72ecd0d2db95d9cfac03e2ed007ee30a
luke connolly <ltconnolly8@gmail.com>
2025-02-13 12:44:38 -0700

    Heartbeat led

--- app/main.c
+++ app/main.c
@@ -3,14 +3,37 @@
 
 int main(void)
 {
-    // Stop watchdog timer
+ 
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
-    
-    // Disable the GPIO power-on default high-impedance mdoe to activate
-    // previously configure port settings
-    PM5CTL0 &= ~LOCKLPM5;
 
-    while(true)
-    {
-    }
+    P1DIR |= BIT0;                          // P1.0 as output
+    P1OUT &= ~BIT0;                         // Clear P1.0
+    
+    PM5CTL0 &= ~LOCKLPM5;                   // GPIO on            
+
+    TB0CTL |= TBCLR;                        // Clear timer and dividers
+    TB0CTL |= TBSSEL__ACLK;                 // Use ACLK                   
+    TB0CTL |= MC__UP;                       // Up counting mode
+    TB0CCR0 = 32768;                        // Compare value
+
+    TB0CCTL0 &= ~CCIFG;                     // Clear CCR0 flag
+    TB0CCTL0 |= CCIE;                       // Enable flag
+    __enable_interrupt(); 
+
+    while(1){}                              // Loop forever
+
+    return 0;
+}
+
+
+//------------------------------------------------------------------------------
+//           Interrupt Service Routines
+//------------------------------------------------------------------------------
+
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void ISR_TB0_CCR0(void)
+{
+    P1OUT ^= BIT0;                          // Toggle led
+    TB0CCTL0 &= ~CCIFG;                     // Clear flag
+
 }
